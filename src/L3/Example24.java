250af0d6dc41b1b1572cb5a9613fd023f3db9801
SudakovAD <sudakovad2023@gamil.com>
2024-01-22 14:37:34 +0500

    123

---
+++ src/L3/Example24.java
@@ -0,0 +1,21 @@
+package L3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Example24 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите два числа");
+
+        int[] num = new int[] {in.nextInt(), in.nextInt()};
+        Arrays.sort(num);
+
+        for (int i = num[0]; i <= num[1]; i++) {
+            System.out.printf("%d ", i);
+        }
+
+        in.close();
+    }
+}
