dd11e8f850244c907be1ac75b2cac80a13eae22b
SudakovAD <sudakovad2023@gamil.com>
2024-01-22 14:04:25 +0500

    123

---
+++ src/L2/E2012.java
@@ -0,0 +1,22 @@
+package L2;
+import java.util.Scanner;
+public class E2012 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int count = in.nextInt();
+
+        if (count < 1 || count > 11) {
+            return;
+        }
+
+        // Если кол. минут для решения оставшихся задач будет больше или равно 0 от кол. минут в 4-х часах, то YES
+        if (4 * 60 - (12 - count) * 45 >= 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        in.close();
+    }
+}
---
+++ src/L2/Example16.java
@@ -0,0 +1,20 @@
+package L2;
+
+import java.util.Scanner;
+
+public class Example16 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите число:");
+        int number = in.nextInt();
+
+        if (number % 3 == 0) {
+            System.out.printf("Число %d делится на 3", number);
+        } else {
+            System.out.printf("Число %d не делится на 3", number);
+        }
+
+        in.close();
+    }
+}
